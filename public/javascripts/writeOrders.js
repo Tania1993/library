e405d3937e8870b0946a0edec0bdeca55442b0af
Tania1993 <t.v.gayuk@gmail.com>
2016-07-13 14:04:32 +0300

    public

---
+++ public/javascripts/writeOrders.js
@@ -0,0 +1,31 @@
+function writeOrders(orders){
+    //p= orders[10].userName
+    alert("ok");
+    for (var i = 0; i < orders.length; i++){
+    tr[i]= orders[i].userName;}
+}
+    /*var rows = orders.books;
+    var cells = 5;
+    var body = document.getElementById("body")[0];
+    var table = document.createElement("table");
+    var tableHead = document.createElement("thead");  
+    var tableBody = document.createElement("tbody");*/
+    
+    //th input(type = "submit" value = "Return book" id = "returnBook")
+    //th input(type = "submit" value = "Delete entry" id = "deleteEntry")
+     
+    
+    
+   
+    //var table = document.createElement('table');
+    //document.body.appendChild(table);
+	//for (i = 0; i < orders.length; i++){
+	//var row = table.insertRow(i);
+   // var name = row.insertCell(orders.userName(i));
+	//var book = row.insertCell(orders.books(i));
+    //var date = row.insertCell(orders.datePipicker(i));
+    
+   // }
+
+    //- for (var key in obj)
+  //p= obj[key]
